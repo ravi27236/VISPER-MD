2138e640700ad09ed52912cc8f9bbd9e98833229
ravi27236 <oshadamd05@gmail.com>
2026-02-06 00:12:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 
-
 module.exports = {
-
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&a1dda106-7cf6-40b5-ac37-78c18759594d' : process.env.SESSION_ID, 
-PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
-SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
-,MONGODB: process.env.MONGODB === undefined ? 'MONGODB: process.env.MONGODB === undefined ? 'mongodb+srv://qu6054:7XGfSRE66Yp7667k@cluster0.p7y66.mongodb.net' : process.env.MONGODB' : process.env.MONGODB,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&a1dda106-7cf6-40b5-ac37-78c18759594d' : process.env.SESSION_ID,
+PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,
+SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME,
+MONGODB: process.env.MONGODB === undefined ? 'mongodb+srv://qu6054:7XGfSRE66Yp7667k@cluster0.p7y66.mongodb.net' : process.env.MONGODB
 };
-
